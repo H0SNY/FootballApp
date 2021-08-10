85e70b618d5bb3ecbd8e7fd98d10c693ac92a000
hosnyhany <es-hosnyhany2022@alexu.edu.eg>
2021-08-10 21:47:02 +0200

    final

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ try{
 	app.use(express.urlencoded({extended : true}));
 	app.use(express.json());
 	app.use(cors({
-		origin : ['http://footballapp.us-east-2.elasticbeanstalk.com' , 'http://localhost:3000']
+		origin : ['http://footballapp.us-east-2.elasticbeanstalk.com' , 'http://H0SNY.us-east-2.elasticbeanstalk.com']
 	}));
 	startConncection(uri);
 	app.get('/' , function(req , res){
